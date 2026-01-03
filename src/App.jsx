3f77bcfc8ea4435dee739f75d6bea5462f03eb5e
Sohail khan <120656530+sohail-68@users.noreply.github.com>
2026-01-03 17:20:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import MainLayout from "./layouts/MainLayout.jsx";
 import "./index.css";
